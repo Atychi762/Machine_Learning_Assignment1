4086adac0c26e27797a84a3abcd68e2efbc889c6
Atychi762 <143887269+Atychi762@users.noreply.github.com>
2025-10-14 11:59:35 +0100

    Removing testing.py and adding start of training for the random forest classifier

--- Training.py
+++ Training.py
@@ -4,5 +4,14 @@ from sklearn.ensemble import RandomForestClassifier as rf
 from sklearn.svm import LinearSVC as svc
 
 training_data = pd.read_csv("wildfires_training.csv")
+testing_data = pd.read_csv("wildfires_test.csv")
 
+training_parameters = training_data.drop(columns=["fire"])
+training_target = training_data["fire"]
+
+testing_parameters = testing_data.drop(columns=["fire"])
+testing_target = testing_data["fire"]
+
+classifier = rf(n_estimators=50,max_depth=5)
+classifier.fit(training_parameters,training_target)
 
